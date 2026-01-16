e6faaffd4679b35d4fc7bc38b60a8ff0a92db36d
Isaev Vladislav <vladislavbibouac@gmail.com>
2026-01-17 00:47:39 +0500

    Добавлена модель Brand и миграции SQLite

--- brands/models.py
+++ brands/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    founded = models.PositiveIntegerField()
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "country", "founded"],
+                name="uniq_brand_name_country_founded"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
